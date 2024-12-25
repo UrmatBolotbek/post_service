6eb9a27cccbfa432db02b820b541db85a086fa95
MunirIsmoilov <ismoilov.munir@gmail.com>
2024-12-25 13:49:46 +0300

    commit: Analysis of purchases of advertising posts

--- src/main/java/faang/school/postservice/dto/bought/AdBoughtEvent.java
+++ src/main/java/faang/school/postservice/dto/bought/AdBoughtEvent.java
@@ -1,5 +1,6 @@
 package faang.school.postservice.dto.bought;
 
+import faang.school.postservice.model.enums.AdverstisingPeriod;
 import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
@@ -11,9 +12,10 @@ import java.time.LocalDateTime;
 @Builder
 public class AdBoughtEvent {
     private Long postId;
-    private Long userIs;
+    private Long userId;
     private Double paymentAmount;
     private Integer duration;
+    private AdverstisingPeriod period;
     private LocalDateTime purchaseTime;
 }
 
