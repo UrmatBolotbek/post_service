e8bc6d03f10f215b1d24a3273e6d2dd88ec83a6e
UrmatBolotbek <urmar9797@gmail.com>
2025-01-25 03:12:10 +0100

    обновил классы

--- src/main/java/faang/school/postservice/mapper/post/PostMapper.java
+++ src/main/java/faang/school/postservice/mapper/post/PostMapper.java
@@ -2,8 +2,8 @@ package faang.school.postservice.mapper.post;
 
 import faang.school.postservice.dto.post.PostRequestDto;
 import faang.school.postservice.dto.post.PostResponseDto;
+import faang.school.postservice.mapper.comment.CommentMapper;
 import faang.school.postservice.mapper.resource.ResourceMapper;
-import faang.school.postservice.model.Comment;
 import faang.school.postservice.model.Like;
 import faang.school.postservice.model.Post;
 import org.mapstruct.Mapper;
@@ -14,13 +14,15 @@ import org.mapstruct.ReportingPolicy;
 import java.util.List;
 
 
-@Mapper(componentModel = "spring", unmappedTargetPolicy = ReportingPolicy.IGNORE, uses = ResourceMapper.class)
+@Mapper(componentModel = "spring",
+        unmappedTargetPolicy = ReportingPolicy.IGNORE,
+        uses = {ResourceMapper.class, CommentMapper.class})
 public interface PostMapper {
 
     @Mapping(target = "images", ignore = true)
     @Mapping(target = "audio", ignore = true)
     @Mapping(source = "likes", target = "likeIds", qualifiedByName = "mapLikesToLikeIds")
-    @Mapping(source = "comments", target = "commentIds", qualifiedByName = "mapCommentsToCommentIds")
+    @Mapping(source = "comments", target = "comments")
     PostResponseDto toDto(Post post);
 
     Post toEntity(PostRequestDto postDto);
@@ -28,7 +30,7 @@ public interface PostMapper {
     @Mapping(target = "images", ignore = true)
     @Mapping(target = "audio", ignore = true)
     @Mapping(source = "likes", target = "likeIds", qualifiedByName = "mapLikesToLikeIds")
-    @Mapping(source = "comments", target = "commentIds", qualifiedByName = "mapCommentsToCommentIds")
+    @Mapping(source = "comments", target = "comments")
     List<PostResponseDto> toListPostDto(List<Post> posts);
 
     @Named("mapLikesToLikeIds")
@@ -38,16 +40,6 @@ public interface PostMapper {
         }
         return likes.stream()
                 .map(Like::getId)
-                .toList();
-    }
-
-    @Named("mapCommentsToCommentIds")
-    default List<Long> mapCommentsToCommentIds(List<Comment> comments) {
-        if (comments == null) {
-            return null;
-        }
-        return comments.stream()
-                .map(Comment::getId)
                 .toList();
     }
 }
